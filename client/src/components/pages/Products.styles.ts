7e1b8fa510a29433726a7c050dbd6ad7a2dc0267
Vimal Madhavan <Vimal.Madhavan@intigral.net>
2022-04-07 21:20:18 +0400

    UI fixes

--- client/src/components/pages/Products.styles.ts
+++ client/src/components/pages/Products.styles.ts
@@ -3,8 +3,14 @@ import { css } from '@emotion/react';
 
 export const ProductsWrapperCSS = css`
     margin-left: 1em;
+    margin-right: 1em;
     margin-bottom: 1em;
     display: flex;
     flex: 1;
     flex-direction: column;
+
+    & > div:nth-child(2) {
+        border: 1px solid #eee;
+        padding: 5px;
+    }
 `;
